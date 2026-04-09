a6fee93b1aca4eeffe0760f9385d76d67f62c082
Abdallah Shadad <abdallhshadad8@gmail.com>
2026-04-09 19:29:35 +0200

    create Auth DTOs

---
+++ Backend/UrlShortenerAPI/DTOs/LoginDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortenerAPI.DTOs
+{
+    public class LoginDto
+    {
+        // public string Username { get; set; };
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+    }
+}
---
+++ Backend/UrlShortenerAPI/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UrlShortenerAPI.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Username { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        [MinLength(6)]
+        public string Password { get; set; } = string.Empty;
+
+    }
+}
